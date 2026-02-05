39dd1d52ff8c9630874225e4906395459d54ff59
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-02-05 09:09:37 +0800

    Implemented Class based view for adding tasks

--- blogpage/urls.py
+++ blogpage/urls.py
@@ -4,7 +4,8 @@ from .views import *
 
 urlpatterns = [
     path(route='', view=index, name='index'),
-    path('list/', task_list, name='task_list')
+    path('list/', task_list, name='task_list'),
+    path('add/', TaskAddView.as_view(), name="task_add")
 ]
 
 app_name = "blogpage"
--- blogpage/views.py
+++ blogpage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import TaskForm
+from django.views.generic import FormView
 
 tasks = []
 
@@ -21,3 +22,12 @@ def task_list(request):
         'form': form,
         'tasks': tasks,
     })
+
+class TaskAddView(FormView):
+    template_name = "blogpage/task_add.html"
+    form_class = TaskForm
+    success_url = "/blogpage/list"
+
+    def form_valid(self, form):
+        tasks.append( (form.cleaned_data['task_name'], form.cleaned_data['task_date']) )
+        return super().form_valid(form)
